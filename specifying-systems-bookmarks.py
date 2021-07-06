f8c5e786568941fc87613668707aba69744156b1
A. Jesse Jiryu Davis <jesse@mongodb.com>
2021-07-05 22:18:55 -0400

    Correctly generate bookmarks for table of contents

--- specifying-systems-bookmarks.py
+++ specifying-systems-bookmarks.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-Generate a Table of Contents for "Specifying Systems".
+"""Generate bookmarks for each part/chapter/section of "Specifying Systems".
 
 The "Specifying Systems" book by Leslie Lamport is distributed as a PDF without
 any "bookmarks" (locations in the PDF that appear in the sidebar of e.g.
@@ -8,13 +7,40 @@ Preview.app): lamport.azurewebsites.net/tla/book.html. This script is a hacky
 but automated means to create a version of the PDF with bookmarks.
 
 Requires Python 3.8+ and the packages listed in requirements.txt.
+
+The structure of the book is:
+
+    Introductory Section 1
+    Introductory Section 2
+      .
+      .
+    Part I - <title>
+        Chapter 1 - <title>
+            1.1 - <title>
+              .
+              .
+            1.n - <title>
+          .
+          .
+    Part II - <title>
+      .
+      .
+    Index
 """
+import argparse
+import os
 import re
+import sys
 from dataclasses import dataclass
 from subprocess import check_call, check_output
 
-# TODO: argparse
-BOOK_PDF = "book-21-07-04.pdf"
+parser = argparse.ArgumentParser(sys.argv[0],
+                                 description=__doc__.split('\n')[0])
+
+parser.add_argument('input_pdf', metavar='INPUT_PDF',
+                    help='Path to a copy of "Specifying Systems"')
+args = parser.parse_args()
+output_pdf = f'{os.path.splitext(args.input_pdf)[0]}_out.pdf'
 
 # Create a recipe file using pdfxmeta, part of pdf.tocgen. Each of the strings
 # below is a chapter/section/etc. heading. The recipe file will contain font
@@ -31,7 +57,7 @@ with open('recipe.toml', 'w+') as recipe_file:
         (27, 5, "Propositional Logic"),
     ]:
         output = check_output(
-            ['pdfxmeta', '-p', str(pageno), '-a', str(level), BOOK_PDF,
+            ['pdfxmeta', '-p', str(pageno), '-a', str(level), args.input_pdf,
              pattern], text=True)
         recipe_file.write(output)
         recipe_file.write('\n')
@@ -39,12 +65,12 @@ with open('recipe.toml', 'w+') as recipe_file:
 # Create the raw Table Of Contents (TOC). Since different heading levels in
 # "Specifying Systems" aren't sufficiently distinguished by font, we'll have to
 # do some more munging.
-toc_raw = check_output(['pdftocgen', '-v', '-r', 'recipe.toml', BOOK_PDF],
+toc_raw = check_output(['pdftocgen', '-v', '-r', 'recipe.toml', args.input_pdf],
                        text=True)
 
 
 @dataclass
-class RawTOCLine:
+class TOCLine:
     title: str
     page: int
     vertical_position: float
@@ -63,64 +89,45 @@ def gen_bookmarks():
             continue
 
         line_match = re.match(r'(\s*)"(.*?)" (\d+) ([\d.]+)', line_text)
-        line = RawTOCLine(title=line_match.group(2),
-                          page=int(line_match.group(3)),
-                          vertical_position=float(line_match.group(4)))
+        line = TOCLine(title=line_match.group(2),
+                       page=int(line_match.group(3)),
+                       vertical_position=float(line_match.group(4)))
+
+        if line.title == 'Specifying Systems':
+            continue
 
         # In "Specifying Systems", a part/chapter begins like:
         #
         #           Part I
         #       Getting Started
         #
-        # pdftocgen makes 2 lines in toc_raw. Buffer the first line, add it to
-        # the second to produce a bookmark titled "Part I - Getting Started".
-        if title_match := re.search(r'"(Part \w+)"', line.title):
+        # pdftocgen makes 2 lines in toc_raw, so we'll first see "Part I" and
+        # then "Getting Started". Buffer the first line, add it to the second to
+        # produce a bookmark titled "Part I - Getting Started".
+        if title_match := re.search(r'Part (\w+)', line.title):
             buffer = title_match.group(0)
-        elif title_match := re.search(r'"(Chapter \d+)"', line.title):
+        elif title_match := re.search(r'Chapter \d+', line.title):
             buffer = title_match.group(0)
+        elif re.search(r'(\d+)\.(\d+)\s+(\w+)', line.title):
+            # This is a section like "1.2 Sets".
+            assert buffer is None
+            line.level = 2
+            yield line
         elif buffer:
-            # If buffer starts with 'Chapter' then indent one level, otherwise
-            # it's a Part which is higher-level and not indented.
-            line.level = 1 if buffer.startswith('Chapter') else 0
+            # If buffer starts with 'Part' then this is top-level, else it's
+            # 'Chapter' and this is level 1.
+            line.level = 0 if buffer.startswith('Part') else 1
             line.title = f'{buffer} - {line.title}'
             buffer = None
             yield line
         else:
-            # Not a chapter or part header. If it's front matter before Part I,
-            # no indent, otherwise it's a section header beneath a chapter.
-            line.level = 0 if part == 0 else 2
+            # Not part, chapter, or section header. Accept default level 0.
             yield line
 
 
 with open('toc.tmp', 'w+') as toc_file:
-    part = chapter = 0
-    buffer = None
-    for line in toc_raw.split('\n'):
-        if not line.strip():
-            continue
+    for toc_line in gen_bookmarks():
+        toc_file.write(toc_line.to_toc_line() + '\n')
 
-        if line == "Specifying Systems":
-            continue
-
-        line_groups = re.match(r'(\s*)"(.*?)" (\d+) ([\d.]+)', line)
-        spaces = line_groups.group(1)
-        indents = len(spaces) // 2
-        title = line_groups.group(2)
-        page = line_groups.group(3)
-        vert = line_groups.group(4)
-
-        if level == 5 and (match := re.search(r'"Part (\w+)"', line)):
-            part = match.group(1)
-            buffer = f'Part {part}'
-        elif level == 5 and (match := re.search(r'"Chapter (\d+)"', line)):
-            chapter = match.group(1)
-            buffer = f'Chapter {chapter}'
-        elif buffer:
-            new_spaces = '    ' if buffer.startswith('Chapter') else ''
-            toc_file.write(f'{new_spaces}"{buffer} - {title}" {page} {vert}\n')
-            buffer = None
-        else:
-            new_spaces = '' if part == 0 else '        '
-            toc_file.write(f'{new_spaces}"{title}" {page} {vert}\n')
-
-check_call(['pdftocio', BOOK_PDF, '-t', 'toc.tmp'])
+check_call(['pdftocio', args.input_pdf, '-t', 'toc.tmp', '-o', output_pdf])
+print(output_pdf)
